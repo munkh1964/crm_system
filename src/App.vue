bb42ebac9add13fc1e53f61826664766fdbc0689
munkh1964 <munkhbayar1127@gmail.com>
2021-08-25 17:24:53 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/app/AppHeader";
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+  components: {
+    AppHeader,
+  },
+};
+</script>
---
+++ src/components/app/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+
+    <v-toolbar app dark class="primary">
+      <v-toolbar-title v-text="'CRM СИСТЕМ'"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn plain>Button1</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-app>
+</template>
+
